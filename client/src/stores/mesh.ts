4268873254ea6f2b2e81b608dbbd03adc5737159
Joel Simon <joelsimon6@gmail.com>
2021-08-31 21:33:53 -0700

    position of the cursor on the mesh, mouse down and shishift down is sent to the server, also visualized on the mesh

--- client/src/stores/mesh.ts
+++ client/src/stores/mesh.ts
@@ -2,16 +2,30 @@ import { readable } from 'svelte/store'
 import type { Readable } from 'svelte/store'
 import { socket } from './socket'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-const loader = new OBJLoader()
+import bunnyOBJ from '../assets/bunny.obj?raw'
+
+const objLoader = new OBJLoader()
 
 type Mesh = any
-export const mesh:Readable<null | Mesh> = readable(null, (set) => {    
-    socket.addEventListener('message', function (event) {
-        const objString = event.data
+
+async function objStringToMesh(objString:string):Promise<Mesh> {
+    return new Promise(resolve => {
         const blob = new Blob([objString], { type: 'text/plain' })
         const url = window.URL.createObjectURL(blob)
-        loader.load(url, (data) => {
-            set(data)
+        objLoader.load(url, (data) => {
+            resolve(data)
         })
+    })
+}
+
+export const mesh:Readable<null | Mesh> = readable(null, (set) => {
+    // Start off with a default bunny for testing.
+    objStringToMesh(bunnyOBJ).then((bunny) => {
+        set(bunny)
+    })
+    
+    socket.addEventListener('message', async (event) => {
+        const objString = event.data
+        set(await objStringToMesh(objString))
     })
 })
